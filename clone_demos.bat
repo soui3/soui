cf75ad6f6a8a67ff84264bde53a5c475aa5c7560
huangjianxiong <huangjianxiong@ainemo.com>
2019-07-24 10:50:02 +0800

    update clone_demos.bat

--- clone_demos.bat
+++ clone_demos.bat
@@ -8,41 +8,8 @@ cd ..
 mkdir soui-demo
 cd soui-demo
 
-call :update_repo TimDemo
-call :update_repo UiEditor
-call :update_repo qqlogin
-call :update_repo 360
-call :update_repo 360Preview
-call :update_repo souispy
-call :update_repo PcManager
-call :update_repo SoTool
-call :update_repo mclistview_demo
-call :update_repo souitest
-call :update_repo VUI
-call :update_repo BesLyric
-call :update_repo QQMain
-call :update_repo FrogPlay
-call :update_repo MultiLangs
-call :update_repo TXMgr
-call :update_repo SLogViewer
-call :update_repo souieditor
-call :update_repo DropDemo
-call :update_repo ThreeClearGame
-call :update_repo baiduwp
-call :update_repo ScreenShotTool
-call :update_repo SoftMaster
-call :update_repo SweepingEdge
-call :update_repo iectrl
-call :update_repo mfc.demo
-call :update_repo CapScreenDemo
-call :update_repo SOUI-CEF
-call :update_repo nsisdemo
-call :update_repo SouiWallPaper
-call :update_repo TczKline
-call :update_repo SIpcDemo
-call :update_repo wxdemo
-call :update_repo sinstar3
-call :update_repo idoudemo
+rem call :update_repo TimDemo
+
 
 SET /p selected=1.是否生成soui_demo.sln工程[1=YES;Other=No]:
 if %selected% neq 1 (
@@ -196,10 +163,10 @@ goto :eof
 :update_repo
 if not exist %1 (
   echo "clone %1"
-  git clone https://github.com/soui-demo/%1.git
+  git clone https://github.com/soui3-demo/%1.git
 ) else (
   echo "pull %1"
   cd %1
-  git pull https://github.com/soui-demo/%1.git
+  git pull https://github.com/soui3-demo/%1.git
   cd ..
 )
