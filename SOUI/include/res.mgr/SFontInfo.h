a73b480cfd60ff8c445ddd4c66f5d0a53acb34b2
setoutsoft <setoutsoft@qq.com>
2019-12-09 08:31:54 +0800

    add SFontInfo.h

---
+++ SOUI/include/res.mgr/SFontInfo.h
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace SOUI
+{
+
+	/**
+	* @union      FONTSTYLE
+	* @brief      FONT的风格
+	* 
+	* Describe    
+	*/
+	union FONTSTYLE{
+		DWORD     dwStyle;  //DWORD版本的风格
+		struct 
+		{
+			DWORD byCharset:8;  //字符集
+			DWORD byWeight :8;  //weight/4
+			DWORD fItalic:1;//斜体标志位
+			DWORD fUnderline:1;//下划线标志位
+			DWORD fBold:1;//粗体标志位
+			DWORD fStrike:1;//删除线标志位
+			DWORD cSize : 12; //字体大小，为short有符号类型
+		}attr;
+
+		FONTSTYLE(DWORD _dwStyle=0):dwStyle(_dwStyle){}
+	}; 
+
+	struct FontInfo
+	{
+		FONTSTYLE style;
+		SStringT strFaceName;
+		SStringT strPropEx;
+	};
+}
