99bbfdd5c6177e95f02bea2ff192f2bfc47914aa
setoutsoft <setoutsoft@qq.com>
2020-03-18 19:08:11 +0800

    add skia font fallback support.

--- components/render-skia/drawtext-skia.h
+++ components/render-skia/drawtext-skia.h
@@ -7,7 +7,7 @@
 class SkTextLayoutEx {
 public:
     //not support for DT_PREFIXONLY
-    void init(const wchar_t text[], size_t length,SkRect rc, const SkPaint &paint,UINT uFormat);
+    void init(const wchar_t text[], size_t length,SkRect rc,  SkPaint &paint,UINT uFormat);
 
     SkRect draw(SkCanvas* canvas);
 
@@ -17,6 +17,9 @@ private:
     SkScalar drawLine(SkCanvas *canvas, SkScalar x, SkScalar y, int iBegin,int iEnd);
 
     void buildLines();
+
+	void drawText(SkCanvas *canvas,const wchar_t* text, size_t length, SkScalar x, SkScalar y,  SkPaint& paint);
+	SkScalar measureText( SkPaint *paint,const wchar_t* text, size_t length);
 
 private:
     SkTDArray<wchar_t> m_text;   //文本内容
@@ -28,8 +31,8 @@ private:
     SkTDArray<LineInfo> m_lines;      //分行索引
     UINT            m_uFormat;    //显示标志
     SkRect          m_rcBound;    //限制矩形
-    const SkPaint  *m_paint;
+    SkPaint  *m_paint;
 };
 
 
-SkRect DrawText_Skia(SkCanvas* canvas,const wchar_t *text,int len,SkRect box,const SkPaint& paint,UINT uFormat);
+SkRect DrawText_Skia(SkCanvas* canvas,const wchar_t *text,int len,SkRect box, SkPaint& paint,UINT uFormat);
