225d3c76fc7a3ab4bf24670d0ae02a584d724222
huangjianxiong1 <huangjianxiong1@yy.com>
2022-10-01 15:49:07 +0800

    fix bug that SScrollBarHandler may div by zero

--- SOUI/src/core/SScrollBarHandler.cpp
+++ SOUI/src/core/SScrollBarHandler.cpp
@@ -41,6 +41,8 @@ namespace SOUI
 		SASSERT(m_pSbHost->GetScrollBarSkin(m_bVert));
 		const SCROLLINFO * pSi = m_pSbHost->GetScrollBarInfo(m_bVert);
 		__int64 nTrackPos=pSi->nTrackPos;
+		if(pSi->nPage==0)
+			return CRect();
 		int nMax=pSi->nMax;
 		if(nMax<pSi->nMin+(int)pSi->nPage-1) nMax=pSi->nMin+pSi->nPage-1;
 
